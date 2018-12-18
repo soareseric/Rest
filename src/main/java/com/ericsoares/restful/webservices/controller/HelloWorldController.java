12f2dd1723eff78c165058c4c644d17c2fd73da7
soareseric <contato.eric.sm@gmail.com>
2018-12-18 10:48:07 -0200

    Simple hello word controller created

---
+++ src/main/java/com/ericsoares/restful/webservices/controller/HelloWorldController.java
@@ -0,0 +1,20 @@
+package com.ericsoares.restful.webservices.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+//Controller 
+@RestController
+public class HelloWorldController {
+
+	// GET
+	// URI - /hello-world
+	// method - "Hello World"
+//	@RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+	@GetMapping(path = "/hello-world")
+	public String helloWorld() {
+		return " Hello World ";
+	}
+}
