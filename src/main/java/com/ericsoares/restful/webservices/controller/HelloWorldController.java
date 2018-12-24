e54737d8f7f34b77fc171d5d2741dfa2809aa262
soareseric <contato.eric.sm@gmail.com>
2018-12-24 10:55:52 -0200

    Hello World Bean with path variable

--- src/main/java/com/ericsoares/restful/webservices/controller/HelloWorldController.java
+++ src/main/java/com/ericsoares/restful/webservices/controller/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.ericsoares.restful.webservices.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ericsoares.restful.webservices.domain.HelloWorldBean;
@@ -24,5 +25,9 @@ public class HelloWorldController {
 		return new HelloWorldBean("Hello world");
 	}
 	
+	@GetMapping(value = "/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello world: %s", name));
+	}
 	
 }
--- src/main/java/com/ericsoares/restful/webservices/domain/HelloWorldBean.java
+++ src/main/java/com/ericsoares/restful/webservices/domain/HelloWorldBean.java
@@ -3,9 +3,15 @@ package com.ericsoares.restful.webservices.domain;
 public class HelloWorldBean {
 
 	private String message;
+//	private String name;
 
 	public HelloWorldBean(String message) {
 		this.setMessage(message);
+	}
+	
+	public HelloWorldBean(String message, String name) {
+		this.setMessage(message);
+//		this.setName(name);
 	}
 
 	public String getMessage() {
@@ -16,9 +22,18 @@ public class HelloWorldBean {
 		this.message = message;
 	}
 
+//	public String getName() {
+//		return name;
+//	}
+//
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+
 	@Override
 	public String toString() {
-		return "message";
+		return "message" +  ": " + "name";
 	}
 
 }
+ 
