0228c209c1ee5279efc90a2e8f0832a34e27302a
soareseric <contato.eric.sm@gmail.com>
2018-12-26 16:57:26 -0200

    Starting versioning - basic approach with URI's

---
+++ src/main/java/com/ericsoares/restful/webservices/domain/PersonV1.java
@@ -0,0 +1,19 @@
+package com.ericsoares.restful.webservices.domain;
+
+public class PersonV1 {
+
+	private String name;
+
+	public PersonV1() {
+		
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
