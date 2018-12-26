906b060565dd950bbd605d6b38b586bc03096788
soareseric <contato.eric.sm@gmail.com>
2018-12-26 14:30:16 -0200

    Internationalization implemented

--- src/main/java/com/ericsoares/restful/webservices/controller/UserController.java
+++ src/main/java/com/ericsoares/restful/webservices/controller/UserController.java
@@ -9,7 +9,6 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.Link;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.ResponseEntity;
