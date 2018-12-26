b8ae4691a3005660e6c7d0b5dc6c38ca55812f76
soareseric <contato.eric.sm@gmail.com>
2018-12-26 10:58:53 -0200

    Implemented generic exception handling for all Controllers

--- src/main/java/com/ericsoares/restful/webservices/exceptions/UserNotFoundException.java
+++ src/main/java/com/ericsoares/restful/webservices/exceptions/UserNotFoundException.java
@@ -1,6 +1,6 @@
 package com.ericsoares.restful.webservices.exceptions;
 
-\public class UserNotFoundException extends RuntimeException {
+public class UserNotFoundException extends RuntimeException {
 
 	public UserNotFoundException(String message) {
 		super(message);
